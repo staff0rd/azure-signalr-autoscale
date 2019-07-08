725f3ac5f9bb39b125ab9ce3289e0f2fb02a97aa
stafford <stafford.williams@gmail.com>
2019-07-09 07:59:23 +1000

    cli

---
+++ src/up.bat
@@ -0,0 +1,9 @@
+az group create -l westus -n azure-signalr
+az signalr create --name autoscale --resource-group azure-signalr --sku Standard_S1
+az signalr create --name autoscale-free --resource-group azure-signalr --sku Free_F1
+az signalr key list -n autoscale-free -g azure-signalr --query primaryConnectionString -o tsv
+az monitor metrics list --resource autoscale-free --resource-group azure-signalr --resource-type Microsoft.SignalRService/SignalR
+
+
+dotnet bin\debug\netcoreapp3.0\BenchmarkServer.dll Azure:SignalR:<connectionStringFromAbove>
+dotnet bin\Debug\netcoreapp3.0\Crankier.dll local --target-url http://localhost:5000/echo
