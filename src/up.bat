d064a43e205d26c46166142967467ff4f8a0f113
stafford <stafford.williams@gmail.com>
2019-07-13 08:56:45 +1000

    fix server

--- src/up.bat
+++ src/up.bat
@@ -3,7 +3,9 @@ az signalr create --name autoscale --resource-group azure-signalr --sku Standard
 az signalr create --name autoscale-free --resource-group azure-signalr --sku Free_F1
 az signalr key list -n autoscale-free -g azure-signalr --query primaryConnectionString -o tsv
 az monitor metrics list --resource autoscale-free --resource-group azure-signalr --resource-type Microsoft.SignalRService/SignalR
+az ad sp create-for-rbac -n "azure-signalr-logic-apps"
+az account show --query id
 
 
-dotnet bin\debug\netcoreapp3.0\BenchmarkServer.dll Azure:SignalR:<connectionStringFromAbove>
+dotnet bin\debug\netcoreapp3.0\BenchmarkServer.dll Azure:SignalR:ConnectionString=<connectionStringFromAbove>
 dotnet bin\Debug\netcoreapp3.0\Crankier.dll local --target-url http://localhost:5000/echo
