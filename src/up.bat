a90ad035e121291728972a455379df22b82cf2ae
Stafford Williams <stafford.williams@gmail.com>
2019-07-13 14:37:29 +1000

    added template

--- src/up.bat
+++ src/up.bat
@@ -1,9 +1,13 @@
 az group create -l westus -n azure-signalr
 az signalr create --name autoscale --resource-group azure-signalr --sku Standard_S1
 az signalr create --name autoscale-free --resource-group azure-signalr --sku Free_F1
-az signalr key list -n autoscale-free -g azure-signalr --query primaryConnectionString -o tsv
+az signalr key list -n autoscale -g azure-signalr --query primaryConnectionString -o tsv
 az monitor metrics list --resource autoscale-free --resource-group azure-signalr --resource-type Microsoft.SignalRService/SignalR
+
+https://blog.jongallant.com/2017/11/azure-rest-apis-postman/
 az ad sp create-for-rbac -n "azure-signalr-logic-apps"
+az ad sp reset-credentials --name "azure-signalr-logic-apps"
+
 az account show --query id
 
 
